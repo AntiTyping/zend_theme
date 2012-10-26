254ca85404a86cacd4057668a1cb0f6573306d54
Andy Pliszka <andy@isp44.com>
2012-10-26 15:25:26 -0400

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zend::Application.config.secret_token = '0db6104e60eca5c208c51a9aad23097b319bbb0e3ada05abd583c3c1e14de23daa29134ac71fb3d034a9789367a44a185b1e0ed4865bc7f82fbaba96be29af9c'
